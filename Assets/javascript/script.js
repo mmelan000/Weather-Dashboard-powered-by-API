30b017ca9d5ae7505d06f8539b0fb5a0f4d64230
Cloud <m.melanson000@gmail.com>
2022-07-12 15:23:39 -0400

    fixed duplicate 5 day bug, added icons representing weather, added UV Index coloring.

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,27 +1,31 @@
-var btn = $('button')
-var formContent = ''
-var currentCity = document.querySelector('#city-name')
-var currentTemp = document.querySelector('#current-temp')
-var currentWind = document.querySelector('#current-wind')
-var currentHumidity = document.querySelector('#current-humidity')
-var currentUV = document.querySelector('#current-uv')
-var fiveDay = document.querySelector('#fiveday')
-var i = 0
+var btn = $('button');
+var formContent = '';
+var currentCity = document.querySelector('#city-name');
+var currentTemp = document.querySelector('#current-temp');
+var currentWind = document.querySelector('#current-wind');
+var currentHumidity = document.querySelector('#current-humidity');
+var currentUV = document.querySelector('#current-uv');
+var fiveDay = document.querySelector('#fiveday');
+var i = 0;
+var UV = '';
 
 function generateFiveDay (input) {
+
     var createCard = document.createElement('div');
     var createCardDate = document.createElement('h3');
     var createCardTemp = document.createElement('p');
     var createCardWind = document.createElement('p');
     var createCardHumidity = document.createElement('p');
+    var createCardIMG = document.createElement('img');    
 
-    // createCard.classList.add('col-md-2')
+    createCardIMG.src = 'http://openweathermap.org/img/w/' + input.list[i].weather[0].icon + '.png';
     createCardHumidity.textContent = 'Humidity: ' + input.list[i].main.humidity;
     createCardWind.textContent = 'Wind: ' + input.list[i].wind.speed + ' MPH';
     createCardTemp.textContent = 'Temp: ' + input.list[i].main.temp + ' F';
     createCardDate.textContent = input.list[i].dt_txt.slice(0, -8);
 
     createCard.appendChild(createCardDate);
+    createCard.appendChild(createCardIMG)
     createCard.appendChild(createCardTemp);
     createCard.appendChild(createCardWind);
     createCard.appendChild(createCardHumidity);
@@ -29,12 +33,35 @@ function generateFiveDay (input) {
 }
 
 function renderCurrentDay (input) {
+    var lat = input.city.coord.lat;
+    var lon = input.city.coord.lon;
+    fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,hourly,daily,alerts&appid=d37f3dc5ec1d208f1cd2ae723d8bebc8')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function (data){
+            console.log(data);
+            UV = data.current.uvi;
+            console.log(UV);
+            currentUV.textContent = UV;
+            if (UV >= 11) {
+                currentUV.classList.add('extreme')
+            } else if (UV >= 8) {
+                currentUV.classList.add('very-high')
+            } else if (UV >= 6) {
+                currentUV.classList.add('high')
+            } else if (UV >= 3) {
+                currentUV.classList.add('moderate')
+            } else {
+                currentUV.classList.add('low')
+            }
+        })
+
     localStorage.setItem('Last City', input.city.name);
     currentCity.textContent = input.city.name + ' ' + input.list[i].dt_txt.slice(0, -8);
-    currentTemp.textContent = 'Temp: ' + input.list[i].main.temp + ' F"';
-    currentWind.textContent = 'Wind: ' + input.list[i].wind.speed + ' MPH';
-    currentHumidity.textContent = 'Humidity: ' + input.list[i].main.humidity;
-    currentUV.textContent = '---';
+    currentTemp.textContent = input.list[i].main.temp + ' F';
+    currentWind.textContent = input.list[i].wind.speed + ' MPH';
+    currentHumidity.textContent = input.list[i].main.humidity;
 }
 
 
@@ -42,6 +69,7 @@ function processData(input) {
     console.log(input);
     console.log(input.city.name);
     console.log(input.list);
+    fiveDay.innerHTML = '';
 
     for (i = 0; i < 6; i++) {
         if (i === 0) {
